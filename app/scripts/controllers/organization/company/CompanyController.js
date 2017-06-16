5fc4e204cb43c22cec79922c67f2467fc8b1a3c9
trigitaltech <venkat@trigital.in>
2017-06-16 18:17:31 +0530

    test master screen

--- app/scripts/controllers/organization/company/CompanyController.js
+++ app/scripts/controllers/organization/company/CompanyController.js
@@ -3,14 +3,15 @@
 		CompanyController: function (scope, resourceFactory, location) {
            scope.company = {};
            
+           resourceFactory.companyResource.get(function (data) {
+        	   scope.company = data;
+           });
+           
            scope.routeTo = function (id) {
                 location.path('/viewcompany/' + id);
             };
 
 
-            resourceFactory.companyResource.get(function (data) {
-                scope.company = data;
-            });
         }
     });
 	gst.ng.application.controller('CompanyController', ['$scope', 'ResourceFactory', '$location', gst.controllers.CompanyController]).run(function ($log) {
